42b9591bf81782ea04c094652867c3928cc669d0
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-02-15 12:50:13 +0300

    Changed the logic in the Stack and StackStatistics classes so as not to violete the principles of encapsulstion

--- Lesson2/SecondHomework.playground/Contents.swift
+++ Lesson2/SecondHomework.playground/Contents.swift
@@ -1,11 +1,16 @@
 /// An object, which is a list of elements with a type integer, organized according to the LIFO principle.
 class Stack {
-    fileprivate var array: [Int] = []
-    fileprivate var topElement: Int?
+    private var array: [Int] = [] {
+        didSet {
+            topElement = array.last
+            minimalValue = array.min()
+        }
+    }
+    private var topElement: Int?
+    private var minimalValue: Int?
     
     func push(element: Int) {
         array.append(element)
-        topElement = element
     }
     
     func pop() -> Int? {
@@ -13,7 +18,6 @@ class Stack {
             return nil
         } else {
             let lastElement = array.removeLast()
-            topElement = array.isEmpty ? nil : array[array.count - 1]
             return lastElement
         }
     }
@@ -21,30 +25,15 @@ class Stack {
     func top() -> Int? {
         return topElement
     }
+    
+    func min() -> Int? {
+        return minimalValue
+    }
 }
 
 /// An object, which is a list of elements with a type integer, organized according to the LIFO principle.
 class StackStatistics: Stack {
-    private var minimalValue: Int?
-    
-    override func push(element: Int) {
-        array.append(element)
-        topElement = element
-        minimalValue = array.min()
-    }
-    
-    override func pop() -> Int? {
-        if array.isEmpty {
-            return nil
-        } else {
-            let lastElement = array.removeLast()
-            topElement = array.isEmpty ? nil : array[array.count - 1]
-            minimalValue = array.min()
-            return lastElement
-        }
-    }
-    
     func minimumElement() -> Int? {
-        return minimalValue
+        return super.min()
     }
 }
