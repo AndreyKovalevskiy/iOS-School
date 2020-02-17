1b9e54b4c71de8e28dd9fb7970755566a888ea1a
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-02-17 21:29:04 +0300

    Added two classes: Node and LinkedList. Changed the logic for getting the minimum value on the stack

--- Lesson2/SecondHomework.playground/Contents.swift
+++ Lesson2/SecondHomework.playground/Contents.swift
@@ -21,26 +21,61 @@ class Stack {
 
 /// An object, which is a list of elements with a type integer, organized according to the LIFO principle.
 class StackStatistics: Stack {
-    private var minimalValues: [Int] = []
+    private var list = LinkedList()
     
     override func push(element: Int) {
-        if minimalValues.isEmpty || element < minimalValues.last! {
-            minimalValues.append(element)
-        } else {
-            minimalValues.append(minimalValues.last!)
-        }
+        list.addNode(value: element)
         super.push(element: element)
     }
     
     override func pop() -> Int? {
-        if !minimalValues.isEmpty {
-            minimalValues.removeLast()
-        }
+        list.removeLast()
         return super.pop()
         
     }
     
     func minimumElement() -> Int? {
-        return minimalValues.last
+        return list.getLast()?.getValue()
+    }
+}
+
+/// An object storing data and a link to an element associated with it
+class Node {
+    private var value: Int?
+    private var previous: Node?
+    
+    init(value: Int, previous: Node?) {
+        self.value = value
+        self.previous = previous
+    }
+    
+    func getValue() -> Int? {
+        return value
+    }
+    
+    func getPrevious() -> Node? {
+        return previous
+    }
+}
+
+/// Аn object consisting of elements of the same type, each of which has a pointer to the previous element
+class LinkedList {
+    private var last: Node?
+    
+    func addNode(value: Int) {
+        let currentValue = last?.getValue()
+        if currentValue == nil || value < currentValue!  {
+            last = Node(value: value, previous: last)
+        } else {
+            last = Node(value: currentValue!, previous: last)
+        }
+    }
+    
+    func removeLast() {
+        last = last?.getPrevious()
+    }
+    
+    func getLast() -> Node? {
+        return last
     }
 }
