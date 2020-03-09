6fafaae0e7a6ccb888f75fed4037afdde14369a9
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-03-09 11:50:45 +0300

    Made changes to the pop() method and corrected the description in the Stack and StackStatistics classes

--- Lesson2/SecondHomework.playground/Contents.swift
+++ Lesson2/SecondHomework.playground/Contents.swift
@@ -1,41 +1,34 @@
-/// An object, which is a list of elements, organized according to the LIFO principle.
+/// Class describing the list of elements organized according to the LIFO principle.
 class Stack<T> {
     private var array: [T] = []
     
-    /// Adds element on the top of the stack
+    /// Adds element on the top of the stack.
     /// - Parameter element: The element to append to the stack.
     /// - Complexity: O(1)
     func push(element: T) {
         array.append(element)
     }
     
-    /// Removes and returns the top element of the stack. Returns nil when the invoking stack is empty.
+    /// Removes and returns the top element of the stack.
     /// - Complexity: O(1)
-    /// - Returns: The top element of the stack or nil.
+    /// - Returns: The top element of the stack if the stack is not empty; otherwise, nil.
     func pop() -> T? {
-        if array.isEmpty {
-            return nil
-        } else {
-            return array.removeLast()
-        }
+        return array.popLast()
     }
     
-    /// Returns the top element of the stack. Returns nil when the invoking stack is empty
+    /// Returns the top element of the stack.
     /// - Complexity: O(1)
-    /// - Returns: The top element of the stack or nil.
+    /// - Returns: The top element of the stack if the stack is not empty; otherwise, nil.
     func top() -> T? {
         return array.last
     }
 }
 
-/// An object, which is a list of elements, organized according to the LIFO principle.
+/// Class describing the list of elements organized according to the LIFO principle.
 /// Each stack element must conform to the Comparable protocol.
 class StackStatistics<T: Comparable>: Stack<T> {
     private var minimalValues: [T] = []
     
-    /// Adds element on the top of the stack
-    /// - Parameter element: The element to append to the stack.
-    /// - Complexity: O(1)
     override func push(element: T) {
         if minimalValues.isEmpty || element < minimalValues.last! {
             minimalValues.append(element)
@@ -45,19 +38,14 @@ class StackStatistics<T: Comparable>: Stack<T> {
         super.push(element: element)
     }
     
-    /// Removes and returns the top element of the stack. Returns nil when the invoking stack is empty.
-    /// - Complexity: O(1)
-    /// - Returns: The top element of the stack or nil.
     override func pop() -> T? {
-        if !minimalValues.isEmpty {
-            minimalValues.removeLast()
-        }
+        minimalValues.popLast()
         return super.pop()
     }
     
-    /// Returns the minimum element in the stack. Returns nil when the invoking stack is empty.
+    /// Returns the minimum element in the stack.
     /// - Complexity: O(1)
-    /// - Returns: The last minimum element of the stack or nil.
+    /// - Returns: The minimum element of the stack if the stack is not empty; otherwise, nil.
     func minimumElement() -> T? {
         return minimalValues.last
     }
