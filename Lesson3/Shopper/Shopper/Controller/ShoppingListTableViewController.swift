c589cf20290c5a0fb1ff83dfd590dd98eb797d1b
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-03-22 13:45:14 +0300

    Deleted method numberOfSections(in:) in the ShoppingListViewController

--- Lesson3/Shopper/Shopper/Controller/ShoppingListTableViewController.swift
+++ Lesson3/Shopper/Shopper/Controller/ShoppingListTableViewController.swift
@@ -24,10 +24,6 @@ class ShoppingListTableViewController: UITableViewController {
 
 extension ShoppingListTableViewController {
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shopping.shoppingList.count
     }
