0289b908fb56b268aaeb6c50bf15a7371fe9aab5
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-04-23 18:08:17 +0300

    Remove empty and excess rows

--- Lesson3/Shopper/Shopper/Application/SceneDelegate.swift
+++ Lesson3/Shopper/Shopper/Application/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  Shopper
-//
-//  Created by Andrey Kovalevskiy on 2/29/20.
-//  Copyright © 2020 Andrey Kovalevskiy. All rights reserved.
-//
-
 import UIKit
 
 @available(iOS 13.0, *)
